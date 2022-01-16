a30f853d6651cf46bad9ceb917edb7d248ce1470
Blackjack <johann.schulze.tast@t-online.de>
2022-01-16 13:02:25 +0100

    start implement hash
    Co-Authored-By: elenagensch <68114173+elenagensch@users.noreply.github.com>

--- src/main/java/join/algorithms/HashEquiJoin.java
+++ src/main/java/join/algorithms/HashEquiJoin.java
@@ -19,7 +19,35 @@ public class HashEquiJoin implements Join {
 	public void join(Relation relation1, int joinAttribute1, Relation relation2, int joinAttribute2,
 			Consumer<Tuple> consumer) {
 		// TODO: hash
+		// M = freeBlocks
+		int freeBlocks = blockManager.getFreeBlockCount();
 
+		Block [] buffers = new Block[freeBlocks - 1];
+
+		for (int i = 0; i < freeBlocks - 1; i++) {
+			buffers[i] = blockManager.getFreeBlock();
+		}
+		//get Tuple
+		for(int b = 0; b < relation1.getBlockCount(); b++) {
+			Block block = relation1.getBlock(b);
+			blockManager.pin(block);
+			while(block.hasNext()) {
+				Tuple tuple = block.Next()
+				int hash = tuple.hashCode();
+				if(!buffers[hash].addTuple(tuple)) {
+					buffers[hash]
+					//write to disk and safe the hash to grab later
+
+					blockmanager.unpin(buffers[hash]);
+					buffers[hash] = blockManager.getFreeBlock();
+					buffers[hash].addTuple(tuple)
+				}
+		
+				}
+		}
+		for(int b = 0; b < buffers.length; b++) {
+			blockManager.unpin(buffers[b]);
+		}
 		NestedLoopEquiJoin nestedLoopJoin = new NestedLoopEquiJoin(blockManager);
 		for (int i = 0; i < numBuckets; ++i) {
 			// TODO: join
